1ffa073cf01ad856aaaa5249e02ac5ba22c4a83d
Wade Shen <swadey@mit.edu>
2014-09-23 21:22:17 -0400

    - clean up overlapping binding

--- test/c2.jl
+++ test/c2.jl
@@ -24,26 +24,26 @@ args = docopt(usage, ARGS, version=v"0.0.1")
 if args["--train"] != nothing
   @info "training with $(args["--train"]) using audio from $(args["--audio"]) with $(args["--gaussians"]) gaussians and $(args["--iterations"]) iterations"
   files = analist(args["--train"], dir = args["--audio"])
-  speech, nonspeech = args["--kmeans"] ? kmeans_train(files, g = int(args["--gaussians"]), iterations = int(args["--iterations"]), sample_size = 700) :
-                                         split_train(files, splits = int(log2(int(args["--gaussians"]))), iterations = int(args["--iterations"]))
+  s, ns = args["--kmeans"] ? kmeans_train(files, g = int(args["--gaussians"]), iterations = int(args["--iterations"]), sample_size = 700) :
+                             split_train(files, splits = int(log2(int(args["--gaussians"]))), iterations = int(args["--iterations"]))
   outf = open(args["--model"], "w")
-  serialize(outf, speech)
-  serialize(outf, nonspeech)
+  serialize(outf, s)
+  serialize(outf, ns)
   close(outf)
 end
 
 # score
 if args["--test"] != nothing
-  f = open(args["--model"], "r")
-  speech    = deserialize(f)
-  nonspeech = deserialize(f)
+  f  = open(args["--model"], "r")
+  s  = deserialize(f)
+  ns = deserialize(f)
   close(f)
 
   files = analist(args["--test"], dir = args["--audio"])
-  threshold, opt_fa, opt_miss, opt_dc, fa, miss, eer, N_speech, N_ns = optimize(files, speech, nonspeech)
+  threshold, opt_fa, opt_miss, opt_dc, fa, miss, eer, N_speech, N_ns = optimize(files, s, ns)
   @info "optimum threshold = $threshold [eer = $eer, fa rate = $opt_fa, miss rate = $opt_miss, decision cost = $opt_dc]"
 
-  FAs, misses, N, decisions = test(files, speech, nonspeech, threshold = threshold)
+  FAs, misses, N, decisions = test(files, s, ns, threshold = threshold)
   @info "FA rate   = $(FAs / N_ns) (N = $FAs / $N_ns)"
   @info "Miss rate = $(misses / N_speech) (N = $misses / $N_speech)"
   @info "N         = $N"
--- test/rats.jl
+++ test/rats.jl
@@ -27,28 +27,28 @@ args = docopt(usage, ARGS, version=v"0.0.1")
 if args["--train"] != nothing
   @info "training with $(args["--train"]) using audio from $(args["--audio"]) with $(args["--gaussians"]) gaussians and $(args["--iterations"]) iterations"
   files = marks(args["--train"], dir = args["--audio"])
-  speech, nonspeech = args["--kmeans"] ? kmeans_train(files, g = int(args["--gaussians"]), iterations = int(args["--iterations"]), 
-                                                      speech_frames = rats_speech, nonspeech_frames = rats_nonspeech) :
-                                         split_train(files, splits = int(log2(int(args["--gaussians"]))), iterations = int(args["--iterations"]), 
-                                                     speech_frames = rats_speech, nonspeech_frames = rats_nonspeech)
+  s, ns = args["--kmeans"] ? kmeans_train(files, g = int(args["--gaussians"]), iterations = int(args["--iterations"]), 
+                                          speech_frames = rats_speech, nonspeech_frames = rats_nonspeech) :
+                             split_train(files, splits = int(log2(int(args["--gaussians"]))), iterations = int(args["--iterations"]), 
+                                         speech_frames = rats_speech, nonspeech_frames = rats_nonspeech)
   outf = open(args["--model"], "w")
-  serialize(outf, speech)
-  serialize(outf, nonspeech)
+  serialize(outf, s)
+  serialize(outf, ns)
   close(outf)
 end
 
 # score
 if args["--test"] != nothing
-  f = open(args["--model"], "r")
-  speech    = deserialize(f)
-  nonspeech = deserialize(f)
+  f  = open(args["--model"], "r")
+  s  = deserialize(f)
+  ns = deserialize(f)
   close(f)
 
   files = marks(args["--test"], dir = args["--audio"])
-  threshold, opt_fa, opt_miss, opt_dc, fa, miss, eer, N_speech, N_ns = optimize(files, speech, nonspeech, speech_mask = S, nonspeech_mask = NS)
+  threshold, opt_fa, opt_miss, opt_dc, fa, miss, eer, N_speech, N_ns = optimize(files, s, ns, speech_mask = S, nonspeech_mask = NS)
   @info "optimum threshold = $threshold [eer = $eer, fa rate = $opt_fa, miss rate = $opt_miss, decision cost = $opt_dc]"
 
-  FAs, misses, N, decisions = test(files, speech, nonspeech, threshold = threshold, speech_mask = S, nonspeech_mask = NS)
+  FAs, misses, N, decisions = test(files, s, ns, threshold = threshold, speech_mask = S, nonspeech_mask = NS)
   @info "FA rate   = $(FAs / N_ns) (N = $FAs / $N_ns)"
   @info "Miss rate = $(misses / N_speech) (N = $misses / $N_speech)"
   @info "N         = $N"
